633960bb235c393f4e336b98a28cbc6bb3e73715
Elliot Craig <ellcraig@iu.edu>
2021-04-27 03:19:10 -0400

    Done for the night, working on Spawning correct player in Town scene

---
+++ RPGFinal/Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject MagePlayer;
+    public GameObject WarriorPlayer;
+    public Transform StartPoint;
+
+    // Start is called before the first frame update
+    void Start(Unit unit)
+    {
+        if (ClassChoice.Mage == true)
+        {
+            Instantiate(MagePlayer, StartPoint);
+        }
+        if (ClassChoice.Warrior == true)
+        {
+            Instantiate(WarriorPlayer, StartPoint);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
