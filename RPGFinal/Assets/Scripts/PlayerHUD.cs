dfae3bbd449e0bab6553557bf550f6e5e7381e8c
Elliot Craig <ellcraig@iu.edu>
2021-04-27 15:23:14 -0400

    Finished script for Player HUD

--- RPGFinal/Assets/Scripts/PlayerHUD.cs
+++ RPGFinal/Assets/Scripts/PlayerHUD.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
+    public GameObject Mage;
+    public GameObject Warrior;
+    private readonly string selectedCharacter = "SelectedCharacter";
+
+    Unit playerUnit;
+
+    public Text nameText;
+    public Text potionsText;
+    public Text manapotsText;
+
+	public Slider hpSlider;
+	public Slider Mana;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int getCharacter;
+
+        getCharacter = PlayerPrefs.GetInt(selectedCharacter);
+
+        switch(getCharacter)
+		{
+			case 1:
+				playerUnit = Mage.GetComponent<Unit>();
+				break;
+			case 2:
+				playerUnit = Warrior.GetComponent<Unit>();
+				break;
+		}
+    }
+    public void SetHUD(Unit unit)
+    {
+        nameText.text = playerUnit.unitName;
+		hpSlider.maxValue = playerUnit.maxHP;
+		hpSlider.value = playerUnit.currentHP;
+		Mana.value = playerUnit.currentMana;
+		Mana.maxValue = playerUnit.maxMana;
+        manapotsText.text = playerUnit.ManaPots.ToString();
+        potionsText.text = playerUnit.Potions.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
