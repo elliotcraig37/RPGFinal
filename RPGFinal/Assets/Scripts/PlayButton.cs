74ac4691554e3ed12758c46ecf845cab13c60006
Elliot Craig <ellcraig@iu.edu>
2021-04-27 00:34:58 -0400

    Working with Unit script in shop

--- RPGFinal/Assets/Scripts/PlayButton.cs
+++ RPGFinal/Assets/Scripts/PlayButton.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {  
+    AddMoney.SetInt("CurrentMoney", 0);
+
     public void loadNextScene() 
     {
         SceneManager.LoadScene("Town");
