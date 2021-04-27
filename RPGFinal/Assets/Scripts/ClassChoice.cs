938ef6770ff954f5d18dbb45f25cf14e5e365b30
Elliot Craig <ellcraig@iu.edu>
2021-04-27 03:09:42 -0400

    Early work on Class Choice Script

---
+++ RPGFinal/Assets/Scripts/ClassChoice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClassChoice : MonoBehaviour
+{
+   public bool Mage;
+   public bool Warrior;
+
+    void Start()
+    {
+        
+    }
+    public void OnMageButton()
+    {
+        Mage = true;
+        Warrior = false;
+        SceneManager.LoadScene("Town");
+    
+    }
+    public void OnWarriorButton()
+    {
+        Mage = false;
+        Warrior = true;
+        SceneManager.LoadScene("Town");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
