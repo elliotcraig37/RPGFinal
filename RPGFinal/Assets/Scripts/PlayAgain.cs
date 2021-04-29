c1a0d9bcaaee0a7033e2716b376f4fc00be8ac11
Elliot Craig <ellcraig@iu.edu>
2021-04-28 21:52:09 -0400

    Gold reset upon death

---
+++ RPGFinal/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    public AddMoney AddMoney;
+
+    public void OnDeathButton()
+    {
+        PlayerPrefs.SetInt("CurrentMoney", 0);
+        SceneManager.LoadScene("Town");
+    }
+}
